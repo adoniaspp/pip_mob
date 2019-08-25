e7dbc59a567ec17cffd3a8d20be3a1a0e3f85856
adoniaspp <adoniaspp@gmail.com>
2019-08-25 10:20:23 -0300

    modalBusca

--- lib/view/v_modal_busca.dart
+++ lib/view/v_modal_busca.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:multiselect_formfield/multiselect_formfield.dart';
+import 'package:dropdown_formfield/dropdown_formfield.dart';
 
 class ModalBusca extends StatefulWidget {
   @override
@@ -11,92 +13,216 @@ class _ModalBuscaState extends State<ModalBusca> {
   String dropDownValueCidades;
   bool checkGaragem = false;
 
+  List _myActivities;
+  String _myActivitiesResult;
+  String _myActivity;
+  String _myActivityResult;
+
+  @override
+  void initState() {
+    super.initState();
+    _myActivities = [];
+    _myActivitiesResult = '';
+    _myActivity = '';
+    _myActivityResult = '';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text("Busca"),
+        centerTitle: true,
       ),
       body: Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
-        child: Column(
-          children: <Widget>[
-            DropdownButton<String>(              
-              isExpanded: true,
-              isDense: true,
-              hint: Text("Finalidade"),
-              iconSize: 40,
-              style: TextStyle(fontSize: 20, color: Colors.black),
-              value: dropDownValueFinalidade,
-              onChanged: (String novoValor){
-                setState(() {
-                  dropDownValueFinalidade = novoValor;
-                });
-              },
-              items: <String>['Aluguel', 'Venda'].map<DropdownMenuItem<String>>((String value){
-                   return DropdownMenuItem<String>(
-                     value: value,
-                     child: Text(value),
-                   );
-                 } 
-              ).toList(),
-            ), 
-            SizedBox(height: 30,),
-            DropdownButton<String>(              
-              isExpanded: true,
-              isDense: true,
-              hint: Text("Tipo"),
-              iconSize: 40,
-              style: TextStyle(fontSize: 20, color: Colors.black),
-              value: dropDownValueTipo,
-              onChanged: (String novoValor){
-                setState(() {
-                  dropDownValueTipo = novoValor;
-                });
-              },
-              items: <String>['Casa', 'Apartamento', 'Apartamento na Planta', 'Prédio Comercial', 'Sala Comercial', 'Terreno'].map<DropdownMenuItem<String>>((String value){
-                   return DropdownMenuItem<String>(
-                     value: value,
-                     child: Text(value),
-                   );
-                 } 
-              ).toList(),
-            ),
-            SizedBox(height: 40,),
-            DropdownButton<String>(              
-              isExpanded: true,
-              isDense: true,
-              hint: Text("Cidades"),
-              iconSize: 40,
-              style: TextStyle(fontSize: 20, color: Colors.black),
-              value: dropDownValueCidades,
-              onChanged: (String novoValor){
-                setState(() {
-                  dropDownValueCidades = novoValor;
-                });
-              },
-              items: <String>['Belém', 'Ananindeua', 'Castanhal', 'Marituba', 'Benevides'].map<DropdownMenuItem<String>>((String value){
-                   return DropdownMenuItem<String>(
-                     value: value,
-                     child: Text(value),
-                   );
-                 } 
-              ).toList(),
-            ),
-            SizedBox(height: 40,),
-            CheckboxListTile(
-              title: const Text("Garagem"),
-              value: checkGaragem,
-              secondary: const Icon(Icons.directions_car),
-              onChanged: (bool value){
-                setState(() {
-                  checkGaragem = checkGaragem ? false : true;
-                });
-              },
-            )
-          ],
-        )
-      ),
+          alignment: Alignment.center,
+          padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+          child: ListView(
+            children: <Widget>[
+              DropDownFormField(
+                titleText: 'Finalidade',
+                hintText: 'Aluguel ou Venda',
+                value: _myActivity,
+                onChanged: (value) {
+                  setState(() {
+                    _myActivity = value;
+                  });
+                },
+                dataSource: [
+                  {
+                    "display": "Aluguel",
+                    "value": "aluguel",
+                  },
+                  {
+                    "display": "Venda",
+                    "value": "venda",
+                  },
+                ],
+                textField: 'display',
+                valueField: 'value',
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              DropDownFormField(
+                titleText: 'Tipo de Imóvel',
+                hintText: 'Selecione um tipo de imóvel',
+                value: _myActivity,
+                onChanged: (value) {
+                  setState(() {
+                    _myActivity = value;
+                  });
+                },
+                dataSource: [
+                  {
+                    "display": "Casa",
+                    "value": "casa",
+                  },
+                  {
+                    "display": "Apartamento",
+                    "value": "apartamento",
+                  },
+                  {
+                    "display": "Apartamento",
+                    "value": "apartamento",
+                  },
+                  {
+                    "display": "Apartamento na Planta",
+                    "value": "apartamentoplanta",
+                  },
+                  {
+                    "display": "Prédio Comercial",
+                    "value": "prediocomercial",
+                  },
+                  {
+                    "display": "Sala Comercial",
+                    "value": "salacomercial",
+                  },
+                  {
+                    "display": "Terreno",
+                    "value": "terreno",
+                  },
+                ],
+                textField: 'display',
+                valueField: 'value',
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              DropDownFormField(
+                titleText: 'Cidade',
+                hintText: 'Selecione uma cidade',
+                value: _myActivity,
+                onChanged: (value) {
+                  setState(() {
+                    _myActivity = value;
+                  });
+                },
+                dataSource: [
+                  {
+                    "display": "Belém",
+                    "value": "belem",
+                  },
+                  {
+                    "display": "Ananindeua",
+                    "value": "ananindeua",
+                  },
+                  {
+                    "display": "Castanhal",
+                    "value": "castanhal",
+                  },
+                  {
+                    "display": "Marituba",
+                    "value": "marituba",
+                  },
+                  {
+                    "display": "Benevides",
+                    "value": "benevides",
+                  },
+                ],
+                textField: 'display',
+                valueField: 'value',
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              
+              MultiSelectFormField(
+                autovalidate: false,
+                titleText: 'Bairros',
+                validator: (value) {
+                  if (value == null || value.length == 0) {
+                    return 'Please select one or more options';
+                  }
+                },
+                dataSource: [
+                  {
+                    "display": "Running",
+                    "value": "Running",
+                  },
+                  {
+                    "display": "Climbing",
+                    "value": "Climbing",
+                  },
+                  {
+                    "display": "Walking",
+                    "value": "Walking",
+                  },
+                  {
+                    "display": "Swimming",
+                    "value": "Swimming",
+                  },
+                  {
+                    "display": "Soccer Practice",
+                    "value": "Soccer Practice",
+                  },
+                  {
+                    "display": "Baseball Practice",
+                    "value": "Baseball Practice",
+                  },
+                  {
+                    "display": "Football Practice",
+                    "value": "Football Practice",
+                  },
+                ],
+                textField: 'display',
+                valueField: 'value',
+                okButtonLabel: 'OK',
+                cancelButtonLabel: 'CANCEL',
+                // required: true,
+                hintText: 'Selecione um ou mais bairros',
+                value: _myActivities,
+                onSaved: (value) {
+                  if (value == null) return;
+                  setState(() {
+                    _myActivities = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              CheckboxListTile(
+                title: const Text("Garagem"),
+                value: checkGaragem,
+                secondary: const Icon(Icons.directions_car),
+                onChanged: (bool value) {
+                  setState(() {
+                    checkGaragem = checkGaragem ? false : true;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              RaisedButton.icon(
+                label: Text("Buscar Anúncio"),
+                onPressed: () {},
+                icon: Icon(Icons.search),
+                color: Colors.blue[100],
+              ),
+            ],
+          )),
     );
   }
 }
